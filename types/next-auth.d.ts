3788bd21a5a14ae3a13f5f9ec99ff50352bdc8e4
Martin-cedres <martinfernandocedres@gmail.com>
2025-08-07 01:08:45 -0300

    agregue next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+// next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    role?: string;
+  }
+  interface Session {
+    user: {
+      role?: string;
+    } & DefaultSession["user"];
+  }
+  interface JWT {
+    role?: string;
+  }
+}
