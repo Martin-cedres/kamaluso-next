2ba0f1a8ac0563e1a42a3d7fddaed2a3b6e44842
Martin-cedres <martinfernandocedres@gmail.com>
2025-08-07 23:57:55 -0300

    Forzar deploy para actualizar URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// C:\Users\LENOVO\Desktop\kamaluso-next-completo\app\page.tsx
+// C:\Users\LENOVO\Desktop\kamaluso-next-completo\app\page.tsx ///
 "use client";
 
 import { useEffect, useState } from "react";
